88ed048614f487f7545e90e0fd0f4ea961ae759e
SamUsachev <semenusachev10@gmail.com>
2023-03-04 14:16:50 +0300

    adding basic components

---
+++ pages/cart.tsx
@@ -0,0 +1,10 @@
+
+const Cart = () => {
+    return (
+        <div>
+            <h1>Корзина товаров</h1>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ pages/favourites.tsx
@@ -0,0 +1,10 @@
+
+const Favourites = () => {
+    return (
+        <div>
+            <h1>Товары, которые вам понравились</h1>
+        </div>
+    );
+};
+
+export default Favourites;
